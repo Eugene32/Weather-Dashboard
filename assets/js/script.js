31d1fec8d96c349c48fa5766ac59279314d35bbe
Reynan Kanindot <ekahiyang@gmail.com>
2021-12-28 11:32:36 +1100

    start structure for detail window

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,28 +18,41 @@ main = document.querySelector('main');
 
 
 
-// Create aside with internal elements
+//Create aside with elements
+
+// Create aside 
 var aside = document.createElement('aside');
 aside.classList.add('aside');
 main.append(aside);
 aside = document.querySelector('aside');
 
+// Create searchForm window
 var searchDiv = document.createElement('div');
 searchDiv.classList.add('search-form');
 searchDiv.setAttribute('id', 'searchFormDiv');
 aside.append(searchDiv);
 var searchFormDiv = document.querySelector('#searchFormDiv');
 
+// Create input element
 var input = document.createElement('input');
 input.classList.add('search-input');
 searchFormDiv.append(input);
 
+
+// Create search button
 var btn = document.createElement('button');
 btn.classList.add('buttons');
 btn.setAttribute('id', 'searchBtn');
 searchFormDiv.append(btn);
 const seartchBtn = document.querySelector('#searchBtn');
 seartchBtn.textContent = 'Search';
+
+// Create history window
+var container = document.createElement('div');
+container.classList.add('search-history');
+container.setAttribute('id','search-history');
+aside.append(container);
+
 
 
 // Create detail window
